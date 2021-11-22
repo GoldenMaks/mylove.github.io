aa4b76a21ddc7c7599b82817b084ea0103890ecb
GoldenMax <makstuk1410@gmail.com>
2021-11-22 22:57:43 +0200

    addd

---
+++ teest.js
@@ -0,0 +1,13 @@
+//let a = document.getElementById("yes")
+
+//let b = document.getElementById("no")
+
+//let c = btn.setAttribute("disabled", true);
+
+
+//function onclickk() {
+    //alert('Ти тютю?! Спробуй ще!');
+//};
+
+//a.onclick = onclickk()
+
